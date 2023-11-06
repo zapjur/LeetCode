912a76e0dee33808e29739b282e457cccdb0b5c9
zapjur <62191962+zapjur@users.noreply.github.com>
2023-11-06 18:45:59 +0100

    Time: 0 ms (100%), Space: 42.5 MB (98.47%) - LeetHub

---
+++ 1185-find-in-mountain-array/1185-find-in-mountain-array.java
@@ -0,0 +1,62 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        
+        int length = mountainArr.length();
+
+        int peakIndex = findPeakIndex(1, length - 2, mountainArr);
+
+        int inIndex = binarySearch(0, peakIndex, target, mountainArr, false);
+        if(mountainArr.get(inIndex) == target) return inIndex;
+
+        int deIndex = binarySearch(peakIndex + 1, length - 1, target, mountainArr, true);
+        if(mountainArr.get(deIndex) == target) return deIndex;
+
+        return -1;
+
+    }
+
+    private int findPeakIndex(int low, int high, MountainArray mountainArr){
+        while(low != high){
+            int mid = low + (high-low)/2;
+            if(mountainArr.get(mid) < mountainArr.get(mid + 1)){
+                low = mid +1;
+            }
+            else{
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    private int binarySearch(int low, int high, int target, MountainArray mountainArr, boolean reversed){
+        while(low != high){
+            int mid = low + (high-low) / 2;
+            if(reversed){
+                if(mountainArr.get(mid) > target){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid;
+                }
+            }
+            else{
+                if(mountainArr.get(mid) < target){
+                    low = mid + 1;
+                }
+                else{
+                    high = mid;
+                }
+            }
+        }
+        return low;
+    }
+}
