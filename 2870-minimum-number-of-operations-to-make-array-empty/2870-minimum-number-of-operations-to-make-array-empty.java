39f678188d409ee89020bede0de747d52868b081
zapjur <62191962+zapjur@users.noreply.github.com>
2024-01-04 18:31:54 +0100

    Time: 19 ms (73.58%), Space: 62.9 MB (14.34%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int count = 0;
+
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int t = entry.getValue();
+
+            if(t == 1) return -1;
+
+            count += t/3;
+
+            if(t % 3 != 0) count++;
+        }
+        return count;
+    }
+}
