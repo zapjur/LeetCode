ff5bc2a70b4d95337ae07cd5fa762ed9378c9443
zapjur <62191962+zapjur@users.noreply.github.com>
2024-10-09 10:09:19 +0200

    Time: 1 ms (74.58%), Space: 2.2 MB (90.68%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.go
@@ -0,0 +1,16 @@
+func minAddToMakeValid(s string) int {
+    close, open := 0, 0;
+
+    for _, c := range s {
+        if c == '(' {
+            open++
+        } else if c == ')' && open > 0 {
+            open--
+        } else {
+            close++
+        }
+    }
+
+    return open + close
+
+}
