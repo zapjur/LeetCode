0fe76321f4bd34f0c2fc421d9f71d141d1e591e4
zapjur <62191962+zapjur@users.noreply.github.com>
2024-10-26 17:50:50 +0200

    Time: 220 ms (91.67%), Space: 82.5 MB (8.33%) - LeetHub

---
+++ 2545-height-of-binary-tree-after-subtree-removal-queries/2545-height-of-binary-tree-after-subtree-removal-queries.go
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func findHeightOfNodes(root *TreeNode, l int, height, levelMaxHeight, levelSecondMaxHeight, level []int) int{
+
+    if root == nil {
+        return 0
+    }
+
+    level[root.Val] = l //level Population / preprocessing
+    height[root.Val] = max(findHeightOfNodes(root.Left, l+1, height, levelMaxHeight,              levelSecondMaxHeight, level), findHeightOfNodes(root.Right, l+1, height, levelMaxHeight, levelSecondMaxHeight, level)) + 1
+
+
+    if levelMaxHeight[l] < height[root.Val]{
+        levelSecondMaxHeight[l] = levelMaxHeight[l]
+        levelMaxHeight[l] = height[root.Val]
+    } else if levelSecondMaxHeight[l] < height[root.Val]{
+        levelSecondMaxHeight[l] = height[root.Val]
+    }
+
+    return height[root.Val]
+}
+func treeQueries(root *TreeNode, queries []int) []int {
+    const n = 1000001
+    height := make([]int, n)
+    levelMaxHeight := make([]int, n)
+    levelSecondMaxHeight := make([]int, n)
+    level := make([]int, n)
+
+    findHeightOfNodes(root, 0, height, levelMaxHeight, levelSecondMaxHeight, level)
+
+    ans := make([]int, len(queries))
+
+    for i, query := range queries{
+        levelOfNode := level[query]
+
+        if levelMaxHeight[levelOfNode] == height[query]{ 
+            ans[i] = levelOfNode + levelSecondMaxHeight[levelOfNode] - 1
+        }else{
+            ans[i] = levelOfNode + levelMaxHeight[levelOfNode] - 1
+        }
+    }
+
+    return ans
+}
